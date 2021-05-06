3844f045f92e5572f5f3e1f1774a58aa787d7467
Hung Tang <hungtang@hotmail.com>
2021-05-05 17:55:36 -0700

    updated .env

--- public/assets/js/Stock.js
+++ public/assets/js/Stock.js
@@ -1,9 +1,11 @@
 // url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo";
 
-
+apikey = "1d8c39832e794358a6b5c2baec3e692e";
 // require('dotenv').config();
 
-const ticker = 'TQQQ';
+// DJI (Dow Jones), IXIC (Nasdaq), GSPC (S&P 500),
+// RUT (Russell 2000), 
+const ticker = 'DJI,IXIC,GSPC,RUT';
 
 var formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
@@ -12,13 +14,13 @@ var formatter = new Intl.NumberFormat('en-US', {
 
 const tickerHtml = document.querySelector('.stock-quote');
 
-let url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${ticker}&apikey=${apikey}`;
+let url = `https://api.twelvedata.com/time_series?symbol=${ticker}&interval=1day&apikey=${apikey}`;
 // let url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${ticker}&apikey=${process.env.API_KEY}`;
 
 const presentTicker = function(data) {
-    let amount = formatter.format(data["Global Quote"]["05. price"]);
-    console.log("Typof amount is: " + typeof(amount));
-    tickerHtml.textContent = amount;
+    let indice = data["DJI"]["values"].close;
+    console.log("Typof amount is: " + typeof(indice));
+    tickerHtml.textContent = indice;
 }
 
 fetch (url).then(function(res) {
@@ -26,7 +28,9 @@ fetch (url).then(function(res) {
         console.log(res);
         console.log("==============");
         res.json().then(function(quote) {
-            console.log(quote["Global Quote"]["05. price"]);
+            console.log(quote);
+            console.log(quote.DJI.values[0].close);
+            // console.log(quote["DJI"].["values"].["close"]);
             presentTicker(quote);
         });
     } else {
