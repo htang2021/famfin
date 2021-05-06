49f9a6fd7439d9b3c9129e29463536b538d0d011
Hung Tang <hungtang@hotmail.com>
2021-05-06 14:45:48 -0700

    added indices.js to return major market indices on main page

--- public/assets/js/Indices.js
+++ public/assets/js/Indices.js
@@ -1,25 +1,21 @@
-// require('dotenv').config();
-apikey = "1d8c39832e794358a6b5c2baec3e692e";
+export function indices() {
+    // DJI (Dow Jones), IXIC (Nasdaq), GSPC (S&P 500),
+    // RUT (Russell 2000), 
+    const ticker = 'DJI,IXIC,GSPC,RUT';
 
-// DJI (Dow Jones), IXIC (Nasdaq), GSPC (S&P 500),
-// RUT (Russell 2000), 
-const ticker = 'DJI,IXIC,GSPC,RUT';
-
-let url = `https://api.twelvedata.com/time_series?symbol=${ticker}&interval=1day&apikey=${apikey}`;
-// let url = `https://api.twelvedata.com/time_series?symbol=${ticker}&interval=1day&apikey=${process.env.API_KEY2}`;
-
-function indices(url) {
+    let url = `https://api.twelvedata.com/time_series?symbol=${ticker}&interval=1day&apikey=1d8c39832e794358a6b5c2baec3e692e`;
+    // let url = `https://api.twelvedata.com/time_series?symbol=${ticker}&interval=1day&apikey=${process.env.API_KEY2}`;
 
     fetch (url).then(function(res) {
         if (res.ok) {
             res.json().then(function(quote) {
+                // console.log(quote);
                 let majorIndices = {
                     DJI: Math.round(quote.DJI.values[0].close),
                     IXIC: Math.round(quote.IXIC.values[0].close),
                     GSPC: Math.round(quote.GSPC.values[0].close),
                     RUT: Math.round(quote.RUT.values[0].close)
                 };
-                // console.log(majorIndices);
                 return majorIndices;
             });
         } else {
@@ -30,6 +26,3 @@ function indices(url) {
         res.json(error);
     });
 }
-
-
-module.exports = indices;
