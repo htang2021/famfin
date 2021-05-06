729348357bc39cd9ae38a0dca898e94080dab857
Clayton Goff <cltngoff@gmail.com>
2021-05-05 18:13:39 -0700

    committing latest to merge to develop

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ const sess = {
 
 app.use(session(sess));
 
-// app.engine("handlebars", hbs.engine);
-// app.set("view engine", "handlebars");
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
