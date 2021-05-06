08bb11f5ff3e974f6e0e2738cd7e64b78d7e45e3
Clayton Goff <cltngoff@gmail.com>
2021-05-06 01:39:57 -0700

    member post, update, delete working

--- controllers/api/member-routes.js
+++ controllers/api/member-routes.js
@@ -3,33 +3,74 @@ const { Member } = require('../../models');
 
 // Delete a family member
 router.delete('/:id', (req, res) => {
-    // delete one member by its `id` value
-    Member.destroy({
-      where: {
-        id: req.params.id
+  // delete one member by its `id` value
+  Member.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbMemberData => {
+      if (!dbMemberData) {
+        res.status(404).json({ message: 'No such family member found.' });
+        return;
       }
+      res.json(dbMemberData);
     })
-      .then(dbMemberData => {
-        if (!dbMemberData) {
-          res.status(404).json({ message: 'No such family member found.' });
-          return;
-        }
-        res.json(dbMemberData);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 router.post('/', (req, res) => {
   Member.create({
     first_name: req.body.first_name,
     last_name: req.body.last_name,
     relationship: req.body.relationship,
-    family_id: req.body.family_id
+    // family_id: req.body.family_id
   })
     .then(dbMemberData => res.json(dbMemberData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.post('/', (req, res) => {
+  Member.create({
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    relationship: req.body.relationship,
+    // family_id: req.body.family_id
+  })
+    .then(dbMemberData => res.json(dbMemberData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.put('/:id', (req, res) => {
+  Member.update(
+    {
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      relationship: req.body.relationship,
+      // family_id: req.body.family_id
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  )
+    .then(dbMemberData => {
+      if (!dbMemberData) {
+        res.status(404).json({ message: 'No such family member.' });
+        return;
+      }
+      res.json(dbMemberData);
+    })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
