247dd6317f73d1857ed49fb87e803162871ec437
Hung Tang <hungtang@hotmail.com>
2021-05-05 19:34:52 -0700

    tweaks

--- public/assets/js/Stock.js
+++ public/assets/js/Stock.js
@@ -29,9 +29,10 @@ fetch (url).then(function(res) {
         console.log("==============");
         res.json().then(function(quote) {
             console.log(quote);
-            console.log(quote.DJI.values[0].close);
-            // console.log(quote["DJI"].["values"].["close"]);
-            presentTicker(quote);
+            console.log("DJI: " + Math.round((quote.DJI.values[0].close)*100)/100);
+            console.log("IXIC: " + quote.IXIC.values[0].close);
+            console.log("GSPC: " + quote.GSPC.values[0].close);
+            console.log("RUT: " + quote.RUT.values[0].close);
         });
     } else {
         alert("Error: " + res.statusText);
