dba07d902c61a8d4b6766495bfea4e3c4b53f5ca
Clayton Goff <cltngoff@gmail.com>
2021-05-09 19:23:34 -0700

    updates existing stocks

--- controllers/api/fund-routes.js
+++ controllers/api/fund-routes.js
@@ -3,47 +3,55 @@ const { Fund } = require('../../models');
 const withAuth = require("../../utils/auth");
 
 router.post('/', (req, res) => {
-    Fund.create({
-        stock_name: req.body.stock_name,
-        quantity: req.body.quantity,
-        initial_cost: req.body.initial_cost,
-        member_id: req.body.member_id
-    })
-        .then(dbFundData => res.json(dbFundData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
-});
-
-// show all and or show one by initial cost 
-// find one on stock id initial cost
-// in front end js, get the new cost of this stock find one + cost of new stock 
-// req.body.initial_cost = previous line
-
-router.put('/:id', (req, res) => {
-    Fund.update(
-        {
-            stock_quantity: req.body.stock_quantity,
-            initial_cost: req.body.initial_cost
-        },
-        {
-            where: {
-                id: req.params.id
-            }
+    Fund.findAll({
+        where: {
+            stock_name: req.body.stock_name,
+            member_id: req.body.member_id
         }
-    )
+    })
         .then(dbFundData => {
-            if (!dbFundData) {
-                res.status(404).json({ message: 'No such fund.' });
-                return;
-            }
-            res.json(dbFundData);
+            const updatedCost = parseFloat(dbFundData[0].dataValues.initial_cost) + req.body.initial_cost;
+            const updatedQuantity = dbFundData[0].dataValues.quantity + req.body.quantity;
+            Fund.update(
+                {
+                    quantity: updatedQuantity,
+                    initial_cost: updatedCost
+                },
+                {
+                    where: {
+                        id: dbFundData[0].dataValues.id
+                    }
+                }
+            )
+                .then(dbFundData => {
+                    if (!dbFundData) {
+                        // res.status(404).json({ message: 'No such fund.' });
+                        // return;
+                    }
+                    res.json(dbFundData);
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.status(500).json(err);
+                })
         })
         .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+            Fund.create({
+                stock_name: req.body.stock_name,
+                quantity: req.body.quantity,
+                initial_cost: req.body.initial_cost,
+                member_id: req.body.member_id
+            })
+                .then(dbFundData => res.json(dbFundData))
+                .catch(err => {
+                    console.log(err);
+                    res.status(500).json(err);
+                });
+        })
+});
+
+router.put('/:id', (req, res) => {
+
 });
 
 router.get('/:id', (req, res) => {
