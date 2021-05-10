4341506ec480b13c4c04c2e3fa0050dcf85ecbab
Graham Hall <ghall89@me.com>
2021-05-09 21:09:29 -0400

    get total stock price

---
+++ public/assets/js/stocks.js
@@ -0,0 +1,48 @@
+const getCurrentPrice = async (ticker) => {
+	await fetch(`/api/quote/${ticker}`)
+	.then(response => response.json())
+	.then(data => {
+		return data;	
+	});
+}
+
+
+const buyStock = async (event) => {
+	event.preventDefault();
+	
+	// const initialCost = await getCurrentPrice(document.querySelector('#stock').value.trim());
+	
+	const response1 = await fetch(`/api/quote/${document.querySelector('#stock').value.trim()}`);
+	const initialCost = await response1.json();
+	
+
+	
+	const buyInput = {
+		stock_name: document.querySelector('#stock').value.trim(),
+		quantity: document.querySelector('#quantity').value,
+		initial_cost: initialCost.quote * document.querySelector('#quantity').value,
+		member_id: document.querySelector('#member-choice').value,
+	};
+	
+
+	console.log(buyInput);
+	
+	if (buyInput.stock_name && buyInput.quantity) {
+
+		
+		const response = await fetch('/api/fund', {
+			method: 'post',
+			body: JSON.stringify(buyInput),
+			headers: { 'Content-Type': 'application/json' }
+		});
+
+		if (response.ok) {
+			// document.location.replace('/dashboard');
+		} else {
+			alert(response.statusText);
+		}
+	}
+	
+}
+
+document.querySelector('#buy-stock').addEventListener('submit', buyStock);
