3ce00068593b5f4a710af91f0c06b77d686a35cf
njthanhtrang <njthanhtrang@users.noreply.github.com>
2021-05-09 20:41:36 -0700

    doughnut

--- public/assets/js/piechart.js
+++ public/assets/js/piechart.js
@@ -4,9 +4,24 @@ fetch("/api/member/family")
     console.log(data);
 
     const labels = [];
+    const quantity = [];
+// find number of unique stocks
+// for each unique stock, create a new obj with an array, loop over each value
+// if value matches one of array stocks, place into corresponding array
 
     for (var i = 0; i < data.length; i++) {
-      labels.push(data[i].first_name + " " + data[i].last_name);
+      let userQuantity = 0;
+
+      for (var j = 0; j < data[i].funds.length; j++) {
+        userQuantity += data[i].funds[j].quantity;
+        //   for loop, loop through data[i], create total variable, store total
+        // quantity.push(data[i].funds[j].quantity);
+        console.log(quantity);
+      }
+      if (userQuantity > 0) {
+        quantity.push(userQuantity);
+        labels.push(data[i].first_name + " " + data[i].last_name + data[i].funds[0].stock_name);
+      }
     }
 
     console.log(labels);
@@ -17,17 +32,18 @@ fetch("/api/member/family")
     //   options: {},
     // };
     const chartData = new Chart(element, {
-      type: "pie",
-      labels: labels,
-      datasets: [
-        {
-          label: "My First dataset",
-          backgroundColor: "rgb(255, 99, 132)",
-          borderColor: "rgb(255, 99, 132)",
-          data: [30, 45],
-        },
-      ],
+      type: "doughnut",
+      data: {
+        labels: labels,
+        datasets: [
+          {
+            label: "My First dataset",
+            backgroundColor: "rgb(255, 99, 132)",
+            borderColor: "rgb(255, 99, 132)",
+            data: quantity,
+          },
+        ],
+      },
     });
-
-    // let myChart = new Chart(document.getElementById("myChart"), config);
+    // var myChart = new Chart(document.getElementById("myChart"), config);
   });
