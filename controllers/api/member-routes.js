702a4bb7db9094174d32c0fbc9c89118ae90666c
Clayton Goff <cltngoff@gmail.com>
2021-05-11 14:24:37 -0700

    added a find one member route with fund model

--- controllers/api/member-routes.js
+++ controllers/api/member-routes.js
@@ -65,25 +65,62 @@ router.put('/:id', (req, res) => {
 
 router.get('/family', (req, res) => {
   Member.findAll({
-      where: {
-          user_id: req.session.user_id
-      },
-      include: {
-        model: Fund
-      }
+    where: {
+      user_id: req.session.user_id
+    },
+    include: {
+      model: Fund
+    }
   })
-      .then(dbMemberData => {
-          if (!dbMemberData) {
-              res.status(404).json({ message: 'No such family member.' });
-              return;
-          }
-          res.json(dbMemberData);
-      })
-      .catch(err => {
-          console.log(err);
-          res.status(500).json(err);
-      });
+    .then(dbMemberData => {
+      if (!dbMemberData) {
+        res.status(404).json({ message: 'No such family member.' });
+        return;
+      }
+      res.json(dbMemberData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // findone member
+router.post('/:id', (req, res) => {
+  Member.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: {
+      model: Fund
+    }
+  })
+    .then(dbMemberData => {
+      if (!dbMemberData) {
+        res.status(404).json({ message: 'No such family member found.' });
+        return;
+      }
+      res.json(dbMemberData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.post('/', (req, res) => {
+  Member.create({
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    relationship: req.body.relationship,
+    is_user: req.body.is_user,
+    user_id: req.session.user_id
+  })
+    .then(dbMemberData => res.json(dbMemberData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;
