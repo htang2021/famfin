67bc3e075378373634e722ebccd549a3399a0ac7
njthanhtrang <njthanhtrang@users.noreply.github.com>
2021-05-10 22:33:35 -0700

    dashboard scripts

--- controllers/api/fund-routes.js
+++ controllers/api/fund-routes.js
@@ -2,21 +2,12 @@ const router = require('express').Router();
 const { Fund } = require('../../models');
 const withAuth = require("../../utils/auth");
 
-<<<<<<< HEAD
 router.post('/', withAuth, (req, res) => {
-    Fund.create({
-        stock_name: req.body.stock_name,
-        quantity: req.body.quantity,
-        initial_cost: req.body.initial_cost,
-        member_id: req.body.member_id
-=======
-router.post('/', (req, res) => {
     Fund.findAll({
         where: {
             stock_name: req.body.stock_name,
             member_id: req.body.member_id
         }
->>>>>>> develop
     })
         .then(dbFundData => {
             const updatedCost = parseFloat(dbFundData[0].dataValues.initial_cost) + req.body.initial_cost;
@@ -59,16 +50,12 @@ router.post('/', (req, res) => {
         })
 });
 
-<<<<<<< HEAD
 // show all and or show one by initial cost 
 // find one on stock id initial cost
 // in front end js, get the new cost of this stock find one + cost of new stock 
 // req.body.initial_cost = previous line
 
 router.put('/:id', withAuth, (req, res) => {
-=======
-router.put('/:id', (req, res) => {
->>>>>>> develop
     Fund.update(
         {
             stock_quantity: req.body.stock_quantity,
