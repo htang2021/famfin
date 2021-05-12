5d40c441f07cb6d6b32fea7c86fc67f099aa458f
Graham Hall <ghall89@me.com>
2021-05-12 20:36:39 +0000

    sell stocks

--- controllers/api/fund-routes.js
+++ controllers/api/fund-routes.js
@@ -53,7 +53,7 @@ router.post('/', (req, res) => {
 router.put('/:id', (req, res) => {
     Fund.update(
         {
-            stock_quantity: req.body.stock_quantity,
+            quantity: req.body.quantity,
             initial_cost: req.body.initial_cost
         },
         {
@@ -79,6 +79,27 @@ router.get('/:id', (req, res) => {
     Fund.findAll({
         where: {
             user_id: req.body.user_id
+        }
+    })
+        .then(dbFundData => {
+            if (!dbFundData) {
+                res.status(404).json({ message: 'No such fund.' });
+                return;
+            }
+            res.json(dbFundData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+//get an individual stock
+
+router.get('/stock/:id', (req, res) => {
+    Fund.findAll({
+        where: {
+            id: req.params.id
         }
     })
         .then(dbFundData => {
