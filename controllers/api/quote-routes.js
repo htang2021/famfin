7ba5878812b9b5c1d14f480c651c97f1f44c0346
Hung Tang <hungtang@hotmail.com>
2021-05-16 12:05:45 -0700

    treat quote minute/day limit

--- controllers/api/quote-routes.js
+++ controllers/api/quote-routes.js
@@ -16,23 +16,24 @@ router.get('/:ticker', async (req, res) => {
     let ticker = req.params.ticker;
 
     // initialize an object to respond to route.get
-    const quoteRes = {
-        ticker,
-        quote: ''
-    };
+    const quoteRes = { ticker, quote: '' };
 
-    // URL to the alphavantage api for stock quote json object
+    // URL to the alphavantage api for stock quote json object (5 requests/minute & 500 requests/day limit)
     let url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${ticker}&apikey=${process.env.API_KEY1}`;
 
     await fetch(url).then(function(response) {
         if (response.ok) {
             response.json().then(function(quote) {
-                // returns the quote in USD of string type $xx.yy
-                console.log(quote["Global Quote"]["05. price"]*100/100);
-                let priceQuote = quote["Global Quote"]["05. price"]*100/100;
-                quoteRes.quote = priceQuote;
-                console.log(quoteRes);
-                res.json(quoteRes);
+                // returns the quote in xx.yy format (with 2 deciaml pts)
+                if (!quote["Note"]) {
+                    let priceQuote = quote["Global Quote"]["05. price"]*100/100;
+                    // Setting price quote to the quote property of quoteRes object
+                    quoteRes.quote = priceQuote;
+                    res.json(quoteRes);
+                } else {
+                    alert("You have hit the query limit!");
+                    return;
+                }
             });
         } else {
             alert("Error: " + response.statusText);
